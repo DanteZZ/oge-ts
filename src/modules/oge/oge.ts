951ebb0d9552d668abfc77571e9c3a6ff070ed90
DantezZ <dantezzstudio@gmail.com>
2021-08-14 17:29:11 +0700

    Обновил импорт EventEmmiter'а

--- src/modules/oge/oge.ts
+++ src/modules/oge/oge.ts
@@ -1,15 +1,19 @@
+import { eventEmitter } from "../eventEmmiter/eventEmmiter";
+
 export class OGE {
-	buffer:Object = {};
-	project:Object = {};
-	FPSLimit:Number = 0;
-	frameStart:Number = Date.now();
-	rootElement:HTMLElement | null = null;
+	public buffer:Object = {};
+	public project:Object = {};
+	public FPSLimit:Number = 0;
+	public frameStart:Number = Date.now();
+	public rootElement:HTMLElement | null = null;
+	public evEm:eventEmitter;
 
 	constructor() {
 		this.buffer = {};
 		this.project = {};
 		this.FPSLimit = 0;
 		this.frameStart = Date.now();
+		this.evEm = new eventEmitter(this);
 	}
 
 	public setElement = (element:HTMLElement | null) => {
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,7 +1,4 @@
 import { rootElement } from "../configs/document.conf";
 import { OGE } from "../modules/oge/oge";
-import { eventEmitter } from "../modules/eventEmmiter/eventEmmiter";
-
 const App = new OGE();
 App.setElement(rootElement);
-const AppEM = new eventEmitter(App);
